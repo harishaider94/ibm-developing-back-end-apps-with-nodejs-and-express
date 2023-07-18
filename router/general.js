ed2634ebec2676a542f45ffaff5c6d5aa5bb0f7c
Haris Haider <haris.haider@arpatech.com>
2023-07-18 15:41:48 +0500

    Task-1 to Task-14

--- router/general.js
+++ router/general.js
@@ -8,6 +8,22 @@ public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books,null,4));
 });
 
+public_users.get('/all', function (req, res) {
+  new Promise((resolve, reject) => {
+    resolve(JSON.stringify(books, null, 4));
+  })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the Promise chain
+      console.error(error);
+      res.status(500).send('An error occurred');
+      reject(error);
+    });
+});
+
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
@@ -19,6 +35,24 @@ public_users.get('/isbn/:isbn',function (req, res) {
       res.status(404).json({ message: 'Book not found' });
     }
  });
+
+ public_users.get('/isbn-2/:isbn',function (req, res) {
+  new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    const filteredBooks = Object.keys(books).filter((key) => books[key].isbn === isbn);
+    const book = books[filteredBooks];  
+    resolve(book);
+  })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the Promise chain
+      console.error(error);
+      res.status(404).json({ message: 'Book not found' });
+      reject(error);
+    });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -32,6 +66,24 @@ public_users.get('/author/:author',function (req, res) {
     }
 });
 
+public_users.get('/author-2/:author',function (req, res) {
+  new Promise((resolve, reject) => {
+    const author = req.params.author;
+    const filteredBooks = Object.keys(books).filter((key) => books[key].author === author);
+    const book = books[filteredBooks];  
+    resolve(book);
+  })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the Promise chain
+      console.error(error);
+      res.status(404).json({ message: 'Book not found' });
+      reject(error);
+    });
+});
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
@@ -43,6 +95,26 @@ public_users.get('/title/:title',function (req, res) {
       res.status(404).json({ message: 'Book not found' });
     }
 });
+
+
+public_users.get('/title-2/:title',function (req, res) {
+  new Promise((resolve, reject) => {
+    const title = req.params.title;
+    const filteredBooks = Object.keys(books).filter((key) => books[key].title === title);
+    const book = books[filteredBooks];  
+    resolve(book);
+  })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the Promise chain
+      console.error(error);
+      res.status(404).json({ message: 'Book not found' });
+      reject(error);
+    });
+});
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
